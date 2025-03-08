83ff960a41a299c25f4e9daee365952c7b2b2557
Vladimir <m.a.x.i.m.o.v@yandex.ru>
2025-03-08 09:43:48 +0300

    расчёт капитала

--- src/ui/components/TotalInformation/Cards/TotalCard.tsx
+++ src/ui/components/TotalInformation/Cards/TotalCard.tsx
@@ -1,9 +1,18 @@
+import { $allAccounts } from '../../../storage/accountStore';
+
+import { useUnit } from 'effector-react';
+
 export default function TotalCard() {
+    const allAccounts = useUnit($allAccounts);
+
+    const calcSumAmount = () =>
+        allAccounts.reduce((acc, curr) => acc + curr.amount, 0);
+
     return(
         <div className='uk-card uk-card-default uk-card-body uk-margin-bottom'>
             <div data-uk-grid>
                 <h1 className='uk-width-expand uk-text-primary'>Всего в наличии</h1>
-                <h1 className='uk-text-primary'>10 000 000 ₽</h1>
+                <h1 className='uk-text-primary'>{calcSumAmount().toFixed(2)}</h1>
             </div>
         </div>
     );
